abafa5b6cc1425829a4a668794bb3b2823e8a72c
matteo <matteo-ferrante@hotmail.it>
2021-11-24 16:53:10 +0100

    some exercise on RNN very basic GAN added get_dict updated saveweights callbacks for Gan gan_celeba.py
    fixed WanbdImagesGAN

--- Generative/utils/callbacks.py
+++ Generative/utils/callbacks.py
@@ -18,14 +18,19 @@ class WandbImagesGAN(keras.callbacks.Callback):
     A custom Callback to produce a grid of images in wandb
     """
 
+    def __init__(self, target_shape):
+        super().__init__()
+        self.target_shape=target_shape
+
     def on_epoch_end(self, epoch, logs=None):
 
-        random_latent_vectors = tf.random.normal(shape=(256, self.model.latent_dim))
+        random_latent_vectors = tf.random.normal(shape=(100, self.model.latent_dim))
         generated_images = self.model.generator(random_latent_vectors)
 
         images = generated_images*255.
-        images = np.repeat(images, 3, axis=-1)
-        vis = build_montages(images, (28, 28), (16, 16))[0]
+        if self.target_shape[-1]==1:
+            images = np.repeat(images, 3, axis=-1)
+        vis = build_montages(images, (self.target_shape[0], self.target_shape[1]), (10, 10))[0]
 
         log={f"image":wandb.Image(vis)}
         wandb.log(log)
