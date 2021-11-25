be4b2d1b54ed125e1e076acb2ab40b360f93b736
matteo <matteo-ferrante@hotmail.it>
2021-11-25 10:04:59 +0100

    gan notebook

--- Generative/gan_celeba.py
+++ Generative/gan_celeba.py
@@ -112,15 +112,8 @@ model_check=SaveGANWeights(filepath="models/gan")
 g.compile()
 
 
-#TODO fix Wandbcallback with specific function
 
-try:
-
-    wb=WandbCallback()
-except:
-    print(f"[WARNING] Something went wrong with wandb callback. Retry..")
-
-    wb=WandbCallback()
+wb=WandbCallback()
 
 callbacks=[
     WandbImagesGAN(target_shape=(128,128,3)),
