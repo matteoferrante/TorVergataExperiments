2f2d47f205bf6fb3a9af74ef07da142ff619ef9f
matteo <matteo-ferrante@hotmail.it>
2021-11-29 13:14:18 +0100

    rimesso come prima PixelCNN.py

--- Generative/classes/PixelCNN.py
+++ Generative/classes/PixelCNN.py
@@ -187,9 +187,9 @@ class PixelCNN(keras.Model):
             x=PixelConvLayer(mask_type="B",filters=128,kernel_size=1,strides=1,activation="relu",padding="valid")(x)
 
 
-        #out=Conv2D(self.num_embeddings,kernel_size=1,strides=1,padding="valid")(x)
+        out=Conv2D(self.num_embeddings,kernel_size=1,strides=1,padding="valid")(x)
 
-        out=Conv2D(1,kernel_size=1,strides=1,padding="valid")(x) #prova modifica
+        #out=Conv2D(1,kernel_size=1,strides=1,padding="valid")(x) #prova modifica
 
 
         pixel_cnn=Model(inputs,out)
