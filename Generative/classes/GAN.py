239eb4dc68237719f05f207088152a5cf9b32a0d
matteo <matteo-ferrante@hotmail.it>
2021-12-02 10:29:04 +0100

    a little bit smaller generator in wgan

--- Generative/classes/GAN.py
+++ Generative/classes/GAN.py
@@ -1,11 +1,32 @@
 import sys
 
 import tensorflow.keras as keras
+import wandb
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.constraints import Constraint
 from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 import tensorflow as tf
 from .Architectures import Decoder,Discriminator
+import tqdm
+from tensorflow.keras import backend
+import numpy as np
 
+
+# clip model weights to a given hypercube
+class ClipConstraint(Constraint):
+    # set clip value when initialized
+    def __init__(self, clip_value):
+        self.clip_value = clip_value
+
+    # clip model weights to hypercube
+    def __call__(self, weights):
+        return backend.clip(weights, -self.clip_value, self.clip_value)
+
+    # get the config
+    def get_config(self):
+        return {'clip_value': self.clip_value}
 
 class GAN(keras.Model):
     """
@@ -236,7 +257,6 @@ class WGAN(keras.Model):
     The discriminator is a critic that tries to maximize the difference
 
     L=D(x)-D(G(z))
-
     while the generator will try to maximize the discriminator's output on synthetic images
 
     L=-D(G(z))
@@ -256,7 +276,7 @@ class WGAN(keras.Model):
 
 
     """
-    def __init__(self,target_shape,latent_dim,encoder_architecture=[(0,128),[(0,256)]], decoder_architecture=[(0,128),[(0,256)]]):
+    def __init__(self,target_shape,latent_dim,d_steps=5,encoder_architecture=[(0,128),[(0,256)]], decoder_architecture=[(0,128),[(0,256)]]):
         """
 
         Attributes
@@ -281,18 +301,21 @@ class WGAN(keras.Model):
 
         self.target_shape = target_shape
         self.latent_dim = latent_dim
-        self.encoder_architecture=encoder_architecture
-        self.decoder_architecture=decoder_architecture
+
+        self.d_steps=d_steps
+
+        #self.encoder_architecture=encoder_architecture
+        #self.decoder_architecture=decoder_architecture
 
 
 
-        #self.discriminator = self.build_discriminator()
-        #self.generator = self.build_generator(latent_dim)
+        self.discriminator = self.build_discriminator()
+        self.generator = self.build_generator(latent_dim)
 
-        self.discriminator=Discriminator(target_shape,1,conv_layer_list=encoder_architecture,activation="linear")
-        self.generator=Decoder(target_shape,latent_dim,decoder_architecture)
+        #self.discriminator=Discriminator(target_shape,1,conv_layer_list=encoder_architecture,activation="linear")
+        #self.generator=Decoder(target_shape,latent_dim,decoder_architecture)
 
-        self.gan=Sequential([self.generator,self.discriminator])
+        self.gan=self.build_gan()
 
 
     def compile(self, d_optimizer=tf.keras.optimizers.Adam(3e-4), g_optimizer=tf.keras.optimizers.Adam(3e-4), loss_fn=tf.keras.losses.BinaryCrossentropy(from_logits=True),d_accuracy=tf.keras.metrics.Accuracy()):
@@ -313,6 +336,170 @@ class WGAN(keras.Model):
         self.discrimnator_loss = loss_fn
         self.d_accuracy=d_accuracy
 
-    def discriminator_loss(y_actual, y_pred):
-        custom_loss = kb.square(y_actual - y_pred)
-        return custom_loss
+    def wesserstein_loss(y_true, y_pred):
+
+        """This should be equivalent to maximize D(x)-D(G(z))"""
+        return tf.reduce_mean(y_true*y_pred)
+
+
+
+
+    def train(self,train_data,steps_per_epoch,epochs=10,val_data=None,validation_steps=None,log=True,callbacks=None):
+
+
+        #callback stuff
+        logs = {}
+
+        if callbacks is not None:
+            callbacks = tf.keras.callbacks.CallbackList(
+                callbacks, add_history=True, model=self)
+
+        if callbacks is not None:
+            callbacks.on_train_begin(logs=logs)
+
+        for epoch in (range(epochs)):
+            print(f"[EPOCH] {epoch}/{epochs}")
+            for _ in tqdm.tqdm(range(steps_per_epoch//self.d_steps)):
+                #train the critic/discriminator
+                d_loss_list=[]
+                d_acc_list=[]
+                g_loss_list=[]
+
+                #train discriminator
+                for x in train_data.take(self.d_steps):
+                    bs=x.shape[0]
+                    d_loss,d_acc=self.train_critic(x)
+
+
+                    d_loss_list.append(d_loss)
+                    d_acc_list.append(d_acc)
+
+                    if log:
+                        wandb.log({"d_loss":d_loss,"d_acc":d_acc})
+
+                #train the generator through the gan with freezed discriminator weights
+
+
+                g_loss=self.train_gan(bs)
+                g_loss_list.append(g_loss)
+                if log:
+                    wandb.log({"g_loss": g_loss})
+                logs={"d_loss":np.mean(d_loss_list),"d_acc":np.mean(d_acc_list),"g_loss":np.mean(g_loss_list)}
+
+            if callbacks is not None:
+                callbacks.on_epoch_end(epoch, logs=logs)
+
+        if callbacks is not None:
+            callbacks.on_train_end(logs=logs)
+
+    def build_gan(self):
+        # make weights in the critic not trainable
+        for layer in self.discriminator.layers:
+            if not isinstance(layer, BatchNormalization):
+                layer.trainable = False
+        # connect them
+        model = Sequential()
+        # add generator
+        model.add(self.generator)
+        # add the critic
+        model.add(self.discriminator)
+        # compile model
+        opt = RMSprop(lr=0.00005)
+        model.compile(loss=WGAN.wesserstein_loss, optimizer=opt)
+        return model
+
+
+    def train_gan(self,bs):
+
+
+        random_latent_vectors = tf.random.normal(shape=(bs, self.latent_dim))
+        misleading_labels= -tf.ones((bs,1))
+
+        g_loss=	self.gan.train_on_batch(random_latent_vectors, misleading_labels)
+        return g_loss
+
+
+
+
+
+    def train_critic(self,x,noisy_labels=True):
+        """Train the discriminator
+        :param x: images
+        :param noisy_labels: bool, if True add some noise to labels to further stabilize training
+        """
+
+        batch_size = tf.shape(x)[0]
+
+        ## step 1 train the discriminator with real and fake imgs
+
+        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
+
+        # Decode them to fake images
+        generated_images = self.generator(random_latent_vectors)
+
+        # Combine them with real images
+        combined_images = tf.concat([generated_images, x], axis=0)
+
+        # Assemble labels discriminating real from fake images
+        labels = tf.concat(
+            [tf.ones((batch_size, 1)), -tf.ones((batch_size, 1))], axis=0
+        )
+
+        if noisy_labels:
+            # Add random noise to the labels - important trick!
+            labels += 0.05 * tf.random.uniform(labels.shape)
+
+        c_loss,c_acc=self.discriminator.train_on_batch(combined_images,labels)
+        return c_loss,c_acc
+
+
+    @staticmethod
+    def build_discriminator():
+        """
+        discriminator model
+
+        :return: discriminator model
+        """
+
+        const=ClipConstraint(0.01)
+        # Create the discriminator
+        discriminator = keras.Sequential(
+            [
+                keras.Input(shape=(28, 28, 1)),
+                layers.Conv2D(128, (3, 3), strides=(2, 2), padding="same",kernel_constraint=const),
+                layers.LeakyReLU(alpha=0.2),
+                layers.Conv2D(256, (3, 3), strides=(2, 2), padding="same",kernel_constraint=const),
+                layers.LeakyReLU(alpha=0.2),
+                layers.GlobalMaxPooling2D(),
+                layers.Dense(1),
+            ],
+            name="discriminator",
+        )
+
+        opt = RMSprop(lr=0.00005)
+        discriminator.compile(loss=WGAN.wesserstein_loss, optimizer=opt,metrics="accuracy")
+        return discriminator
+
+    @staticmethod
+    def build_generator(latent_dim=128):
+        """
+
+        :param latent_dim: dimension of the latent space
+        :return: generator model
+        """
+        generator = keras.Sequential(
+            [
+                keras.Input(shape=(latent_dim,)),
+                # We want to generate 128 coefficients to reshape into a 7x7x64 map
+                layers.Dense(7 * 7 * 64),
+                layers.LeakyReLU(alpha=0.2),
+                layers.Reshape((7, 7, 64)),
+                layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"),
+                layers.LeakyReLU(alpha=0.2),
+                layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding="same"),
+                layers.LeakyReLU(alpha=0.2),
+                layers.Conv2D(1, (7, 7), padding="same", activation="sigmoid"),
+            ],
+            name="generator",
+        )
+        return generator
